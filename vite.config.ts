2cca3f312d15958298e7c4bfbcaff680a7909b5c
Erik Gylleus <144366622+ergyl@users.noreply.github.com>
2025-02-16 22:14:30 +0100

    color for selected text in body

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,23 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import {fileURLToPath, URL} from 'node:url'
+import {defineConfig, PluginOption} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    vue(),
-    ...(mode === 'development' ? [require('vite-plugin-vue-devtools').default()] : []), // Only include vueDevTools in dev mode
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-  base: '/',
-}));
+export default defineConfig(async ({mode}) => {
+    const plugins: PluginOption[] = [vue(), tailwindcss()];
+
+    if (mode === 'development') {
+        const vueDevTools = (await import('vite-plugin-vue-devtools')).default;
+        plugins.push(vueDevTools() as PluginOption);  // Type it as PluginOption
+    }
+
+    return {
+        plugins,
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            },
+        },
+        base: '/',
+    };
+});
