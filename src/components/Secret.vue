50c62166f85a90ee28a636302d781164115188a5
Erik Gylleus <erik.gylleus@consilumsafety.com>
2025-04-17 12:58:32 +0200

    fix update text for secret

--- src/components/Secret.vue
+++ src/components/Secret.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="flex flex-col bg-base-100 max-w-2xl mx-auto items-center text-center justify-center gap-4 mt-8 mb-12 py-8">
+  <div
+      class="flex flex-col bg-base-100 max-w-2xl mx-auto items-center text-center justify-center gap-4 mt-8 mb-12 py-8">
     <img :src="SecretImage" alt="Cookie guy" class="w-20 md:w-30 h-auto"/>
     <h2>You found a secret!</h2>
+    <p>The secret is this dancing flamingo.</p>
     <p>To show the website again, click the reset button below.</p>
-    <Button text="Reset" :rounded="true" @click="resetWindows" ></Button>
+    <Button text="Reset" :rounded="true" @click="resetWindows"></Button>
   </div>
 </template>
 
