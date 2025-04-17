6487ed078cfa5200eef940e3177ad9b0e563be44
Erik Gylleus <erik.gylleus@consilumsafety.com>
2025-04-17 12:24:27 +0200

    refactor App.vue and ExpandableContent.vue and add TODO window

--- src/App.vue
+++ src/App.vue
@@ -15,23 +15,33 @@
         <Window id="about" header-title="about.txt" body-title="About me"
                 @update:hidden="updateWindowState('about', $event)">
           <ExpandableContent>
-            <p> I'm Erik, a junior software developer based in Gothenburg, Sweden. Coding is my passion, and I'm
-              dedicated
-              to constantly improving through continuous learning. My journey in tech started with a curiosity about how
-              computers work, and now I'm turning that curiosity into a career.
+            <p> I'm Erik, a software developer based in Gothenburg, Sweden. Coding is my passion, and I'm dedicated to
+              constantly improving through continuous learning. I'm mostly into back end development and specifically C#
+              and Java, but I'm open to all opportunities to branch out.
             </p>
-            <p>
+            <p class="pt-5">
+              My roadmap for software development:
+              <br>
+              <span class="text-accent">"General Chuck Yeager — the first person to fly faster than sound—praised an airplane's engine system with
+              the words: `simple, few parts, easy to maintain, very strong`".</span>
+              <br><i>― Programming Pearls (1999) by Jon Bentley</i>
+            </p>
+            <p class="pt-5">
               I love coding and think of ways to improve my skillset all the time. For me, it's a creative outlet that I
-              didn't know I had longed for all my life. I'm mostly into back end development and specifically C# and
-              Java,
-              but I'm open to all opportunities to branch out.
+              didn't know I'd been craving for most of my life.
             </p>
-            <p>
+            <p class="pt-5">
+              My interest in tech started early, much thanks to my dad who worked in IT while I was growing up. I spent
+              a lot of time playing computer games and experimenting on my computer — whether it was making 3D models
+              and textures, building simple websites, or tweaking CSS just for fun. Now I'm turning that curiosity into
+              a career. Never stop learning, right?
+            </p>
+            <p class="pt-5">
               I see myself an enabler. I like to help. I work hard. I really love learning new things and strongly
               believe
               in sharing knowledge, teaching each other those new things we learn.
             </p>
-            <p>
+            <p class="pt-5">
               Some other things that I think are fun are: gaming, cooking, and travelling.
             </p>
           </ExpandableContent>
@@ -41,6 +51,22 @@
         </Window>
         <Window id="projects" header-title="projects/" @update:hidden="updateWindowState('projects', $event)">
           <ProjectsOverview/>
+        </Window>
+        <Window id="todo" header-title="TODO.txt" body-title="MY TODO LIST">
+          <p>These are things I wanna do in my spare time. Some ideas for personal projects I've written down to dig
+            into. :)</p>
+          <ul class="list-disc px-4">
+            <li>Complete the <a href="https://github.com/theowni/Damn-Vulnerable-RESTaurant-API-Game" rel="external"
+                                target="_blank">Damn
+              Vulnerable RESTaurant</a> challenge to practise identifying and fixing vulnerabilities
+            </li>
+            <li>Build a simple AI chatbot with web speech API and Node.js</li>
+            <li>Create a CLI tool</li>
+            <li>Build a CDN from scratch using Lua</li>
+            <li>Build a Promise in JavaScript from scratch</li>
+            <li>Implement Bloom filter to learn how the data structure works under the hood</li>
+            <li>Create a Emulator/Virtual machine (maybe?)</li>
+          </ul>
         </Window>
         <Window id="resume" header-title="resume.md" body-title="Checkout my CV"
                 @update:hidden="updateWindowState('resume', $event)">
